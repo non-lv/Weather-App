3aab92816442cd16dfe6722dcd621a4a2ca80732
non <44144189+non-lv@users.noreply.github.com>
2024-09-16 19:56:45 +0300

    graphjs

--- Weather-App/weather-app.client/src/App.tsx
+++ Weather-App/weather-app.client/src/App.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, registerables } from 'chart.js/auto';
+import 'chartjs-adapter-moment';
 import './App.css';
+
+ChartJS.register(...registerables);
 
 interface Forecast {
     country: string;
@@ -7,7 +12,7 @@ interface Forecast {
     temp: number;
     tempMin: number;
     tempMax: number;
-    timestamp: string;
+    timestamp: Date;
 }
 
 interface Location {
@@ -50,31 +55,71 @@ function App() {
             });
 
             return <div>{places.map(p =>
-                <div>
-                    <h2>{p.country} - {p.city}</h2>
-                    <table className="table table-striped" aria-labelledby="tableLabel">
-                        <thead>
-                            <tr>
-                                <th>Date</th>
-                                <th>Temperature</th>
-                                <th>Min Temperature</th>
-                                <th>Max Temperature</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {filter(p, forecasts).map(forecast =>
-                                <tr key={forecast.timestamp}>
-                                    <td>{forecast.timestamp}</td>
-                                    <td>{forecast.temp}</td>
-                                    <td>{forecast.tempMin}</td>
-                                    <td>{forecast.tempMax}</td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
+                <div style={{ width: "70vw", height: "30vw" }} >
+                    {createGraph(p, filter(p, forecasts))}
                 </div>)}
             </div>;
         }
+    }
+
+    function createGraph(location: Location, forecasts: Forecast[]) {
+        const dates: number[] = forecasts.map(f => (new Date(f.timestamp)).getTime());
+        const lastUpdate: Date = new Date(Math.max(...dates));
+
+        return <Line data={{
+            labels: forecasts.map(forecast => forecast.timestamp),
+            datasets: [{
+                label: "Temperature",
+                data: forecasts.map(forecast => forecast.temp),
+                backgroundColor: "#E05C5C",
+                borderColor: "#E05C5C"
+            },
+            {
+                label: "Min",
+                data: forecasts.map(forecast => forecast.tempMin),
+                backgroundColor: "#E05C5C",
+                borderColor: "#E05C5C"
+            },
+            {
+                label: "Max",
+                data: forecasts.map(forecast => forecast.tempMax),
+                backgroundColor: "#E05C5C",
+                borderColor: "#E05C5C"
+            }],
+        }}
+            options={{
+                responsive: true,
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'second',
+                            tooltipFormat: 'mm:ss',
+                        },
+                        ticks: {
+                            major: {
+                                enabled: true,
+                            },
+                        },
+                    },
+                    y: {
+                        type: 'linear',
+                        suggestedMin: 0,
+                        suggestedMax: 20
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: location.country + " - " + location.city
+                    },
+                    subtitle: {
+                        display: true,
+                        text: "Last Update: " + lastUpdate
+                    }
+                }
+            }}
+        />;
     }
 
     async function populateWeatherData() {
